c0f10a0df87a4a90bfa665220bf40e40ab736872
Oxygenxy <572535121@qq.com>
2018-03-03 16:14:03 +0000

    MyEDF 0.02
    reduced

--- +torsche/EDF.m
+++ +torsche/EDF.m
@@ -5,20 +5,18 @@ ts=colour(ts);
 
 c    =  ts.ProcTime;
 per  =  ts.Period;
-
-noft=size(c,2);
-
-tmax=0;
-tstop=1;
 remainProc = c;
+
+noft = size(c,2);
+
+tmax = 0;
+tstop = 1;
+
 
 for i = 1:noft
     tstop = lcm(tstop, per(i));
 end
 
-%ddleLower = (fix(tamx./per)).*per;
-%ddleUpper = (fix(tamx./per)+1).*per;
-%[minDdle,indexDdle]=min((fix(tmax./per)+1).*per);
 for i = 1:noft
 
     pt = ts.tasks(i);
@@ -31,119 +29,85 @@ for i = 1:noft
     
 end
 
-function add_to_ready(j)
-    
-    readyTasks{j}.Processor = j; % Hack for plotting the schedule
-    if(fix(tmax/per(j))==0)
-        readyTasks{j}.ReleaseTime = tmax;
-    end
-    readyTasks{j}.Deadline = tempDdle(j); 
-end
 
 function I = findEqual(x,k)
-        I=[];
-        col=size(x,2);
-        for i=1:col
+        I = [];
+        col = size(x,2);
+        for i = 1:col
             if(x(i)==k)
-                I=[I i];
+                I = [I i];
             end
         end
 end
 
+
 function addResultts(j)
         if isempty(resultts)
-            readyTasks{j}.Deadline=tempDdle(j);
-            readyTasks{j}.ReleaseTime=tempDdle(j)-per(j);
+            readyTasks{j}.Deadline = tempDdle(j);
+            readyTasks{j}.ReleaseTime = tempDdle(j)-per(j);
             resultts = [readyTasks{j}];
         else
-            readyTasks{j}.Deadline=tempDdle(j);
-            readyTasks{j}.ReleaseTime=tempDdle(j)-per(j);
+            readyTasks{j}.Deadline = tempDdle(j);
+            readyTasks{j}.ReleaseTime = tempDdle(j)-per(j);
             resultts = [resultts readyTasks{j}];
         end
+end
+
+function getAddScht(j,process)
+    
+        [start, len, processor] = get_scht(readyTasks{j});
+     
+         start = [start tmax];
+         len = [len process];
+         processor = [processor readyTasks{j}.Processor];
+         
+         readyTasks{j} = add_scht(readyTasks{j}, start, len, processor);
+end
+
+function setDdleRemainProc()
+         I = findEqual(tempDdle,ddleResult(1));
+         for k = 1:size(I,2)
+            index = I(k);
+            remainProc(index) = c(index);
+            addResultts(index);
+         end
 end
 
 while(tmax<tstop)
 
 tempDdle = (fix(tmax./per)+1).*per;
-[ddleResult,ddleIndex]=sort(tempDdle,2,'ascend');
+[ddleResult,ddleIndex] = sort(tempDdle,2,'ascend');
 
 
-%tempProced=tmax-ddleLower;
-%remainProc=c-tempProced;
+    for i = 1:noft
+        j = ddleIndex(i);
+        edRem = remainProc(j);
+        tmaxPlus = tmax+edRem;
+        if(~edRem) 
+            continue;
+        elseif(tmaxPlus<ddleResult(1))
 
-for i=1:noft
-    j = ddleIndex(i);
-    edRem = remainProc(j);
-    tmaxPlus = tmax+edRem;
-   if(~edRem) 
-       continue;
-   elseif(tmaxPlus<=ddleResult(1))
+            getAddScht(j,edRem);
+            remainProc(j) = 0;
+            tmax = tmax+edRem;
        
-       add_to_ready(j);
-       [start, len, processor] = get_scht(readyTasks{j});
-      
-       
-       start=[start tmax];
-       len=[len edRem];
-       processor = [processor readyTasks{j}.Processor];
-       readyTasks{j} = add_scht(readyTasks{j}, start, len, processor);
-       
-       tmax = tmax+edRem;
-       remainProc(j) = 0;
-       
-
-%       if isempty(resultts)
-%           resultts = [readyTasks{j}];
-%       else
-%            resultts = [resultts readyTasks{j}];
-%       end
-       
-       if(tmaxPlus==ddleResult(1))
-           I=findEqual(tempDdle,ddleResult(1));
-           for k=1:size(I,2)
-               index=I(k);
-               remainProc(index) = c(index);
-               addResultts(index);
-           end
-           break;
-       end
-       
-       continue;
+            continue;
    
-   elseif(tmaxPlus>ddleResult(1))
-       
-       add_to_ready(j);
-       [start, len, processor] = get_scht(readyTasks{j});
-       
-       
-       start=[start tmax];
-       len=[len (ddleResult(1)-tmax)];
-       processor = [processor readyTasks{j}.Processor];
-       readyTasks{j} = add_scht(readyTasks{j}, start, len, processor);
-       
-       remainProc(j) = remainProc(j)-(ddleResult(1)-tmax);
-       tmax = ddleResult(1);
-       
-        I=findEqual(tempDdle,ddleResult(1));
-        for k=1:size(I,2)
-               index=I(k);
-               remainProc(index) = c(index);
-               addResultts(index);
+        elseif(tmaxPlus>=ddleResult(1))
+      
+            getAddScht(j,(ddleResult(1)-tmax));       
+            remainProc(j) = remainProc(j)-(ddleResult(1)-tmax);
+            tmax = ddleResult(1);
+            setDdleRemainProc();
+            
+            break;
         end
-       %remainProc(ddleIndex(1)) = c(ddleIndex(1));
-       break;
-   end
-end
+    end
 
-if(tmax<ddleResult(1))
-    tmax=ddleResult(1);
-    I=findEqual(tempDdle,ddleResult(1));
-        for k=1:size(I,2)
-               index=I(k);
-               remainProc(index) = c(index);
-               addResultts(index);
-        end
-end
+    if(tmax<ddleResult(1))
+        tmax = ddleResult(1);
+        setDdleRemainProc();
+    end
         
 
 end
